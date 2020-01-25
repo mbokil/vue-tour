27069acbb8b9f563d29390181bab9ba1a0b2c267
Mark Bokil <mark.bokil@fmc-na.com>
2020-01-25 15:29:05 -0500

    fixes for IE11
    fixes for IE11

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/app', {
+      'targets': {
+        'browsers': [
+            '> 1%',
+            'last 2 versions',
+            'ie >= 11'
+          ]
+        },
+        'useBuiltIns': 'entry'
+    }]
   ]
 }
